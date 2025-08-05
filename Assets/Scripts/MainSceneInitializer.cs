7c52754396d2af26843a8e97bc36860508ac5b18
Aymen Al-Alimi <a7yan95@gmail.com>
2025-08-06 02:52:57 +0300

    Update MainSceneInitializer.cs

--- Assets/Scripts/MainSceneInitializer.cs
+++ Assets/Scripts/MainSceneInitializer.cs
@@ -4,11 +4,23 @@ public class MainSceneInitializer : MonoBehaviour
 {
     void Start()
     {
-        // إنشاء الغرفة
-        GameObject room = new GameObject("BasicRoom");
-        room.AddComponent<BasicRoom>();
+        CreateRoom();
+        SpawnNewAdam();
 
-        // إنشاء آدم الجديد
+        // أضف هذا السطر فقط
+        gameObject.AddComponent<CameraSetup>();
+    }
+
+    void CreateRoom()
+    {
+        GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        floor.transform.localScale = new Vector3(10, 0.1f, 10);
+        floor.transform.position = new Vector3(0, -0.05f, 0);
+        floor.name = "Floor";
+    }
+
+    void SpawnNewAdam()
+    {
         GameObject adam = GameObject.CreatePrimitive(PrimitiveType.Capsule);
         adam.transform.position = new Vector3(0, 1, 0);
         adam.name = "NewAdam";

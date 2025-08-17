4230f4998b582d8aef68a33449edf1c71587eff9
Aymen Al-Alimi <a7yan95@gmail.com>
2025-08-17 03:38:50 +0300

    Update HotWall.cs

--- world/HotWall.cs
+++ world/HotWall.cs
@@ -7,14 +7,21 @@ public class HotWall : MonoBehaviour
         gameObject.name = "HotWall";
     }
 
-    void OnCollisionEnter(Collision collision)
+   void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "NewAdam")
         {
             var response = collision.gameObject.GetComponent<AdamResponse>();
+            var memory = collision.gameObject.GetComponent<ShortTermMemory>();
+    
             if (response != null)
             {
                 response.ReactTo("Burned by HotWall");
+            }
+    
+            if (memory != null)
+            {
+                memory.RecordEvent("Touched HotWall", Time.time.ToString("F2"));
             }
         }
     }
